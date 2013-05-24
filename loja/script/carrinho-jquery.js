35fd8e83ab9bff80cf88570b6ec0cebc790a7312
Francisco <f.thiene@gmail.com>
2013-05-24 11:47:42 -0300

    Finish Exercise 6-10

--- loja/script/carrinho-jquery.js
+++ loja/script/carrinho-jquery.js
@@ -10,3 +10,25 @@ $("#carrinho").on("click", "li form p a", function(event){
   
   event.preventDefault();
 });
+
+$("#carrinho li span[id$=valor_unit]").each(function(){
+  var spanValorUnitario = $(this);
+  
+  var valorUnitario = realParaNumber(spanValorUnitario.text());
+  spanValorUnitario.data("valor", valorUnitario);
+});
+
+$("#carrinho").on("keyup", "li input[type=text]", function(event){
+  var input = $(this);
+  var li = input.closest("li");
+  var spanValorUnit = li.find("span[id$=valor_unit]");
+  var spanValorTotal = li.find("span[id$=valor_total]");
+
+  var quantidade = parseInt(input.val());
+  var valorUnitario = spanValorUnit.data("valor");
+
+  if(!quantidade) quantidade = 0;
+
+  var total = quantidade * valorUnitario;
+  spanValorTotal.text(numberParaReal(total));
+});
--- loja/script/carrinho.js
+++ loja/script/carrinho.js
@@ -1,18 +1,18 @@
-var carrinho = document.getElementById("carrinho");
-var inputs = carrinho.getElementsByTagName("input");
+// var carrinho = document.getElementById("carrinho");
+// var inputs = carrinho.getElementsByTagName("input");
 
-for(var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("keyup", function(event) {
-    var li = this.parentNode.parentNode.parentNode;
-    var spans = li.getElementsByTagName("span");
+// for(var i = 0; i < inputs.length; i++) {
+//   inputs[i].addEventListener("keyup", function(event) {
+//     var li = this.parentNode.parentNode.parentNode;
+//     var spans = li.getElementsByTagName("span");
     
-    var spanValorUnitario = spans[0];
-    var spanValorTotal = spans[1];
+//     var spanValorUnitario = spans[0];
+//     var spanValorTotal = spans[1];
     
-    var quantidade = this.value;
-    var valorUnitario = realParaNumber(spanValorUnitario.textContent);
-    var novoTotal = quantidade * valorUnitario;
+//     var quantidade = this.value;
+//     var valorUnitario = realParaNumber(spanValorUnitario.textContent);
+//     var novoTotal = quantidade * valorUnitario;
 
-    spanValorTotal.textContent = numberParaReal(novoTotal);
-  });
-}
+//     spanValorTotal.textContent = numberParaReal(novoTotal);
+//   });
+// }
