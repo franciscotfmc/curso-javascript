52f1352ff9c00958a251ed85c5ac84d0ec69f065
Francisco <f.thiene@gmail.com>
2013-05-24 10:51:18 -0300

    Finish Exercise 6-8

--- loja/script/carrinho-jquery.js
+++ loja/script/carrinho-jquery.js
@@ -1,1 +1,12 @@
 $("#carrinho li form a").show();
+
+$("#carrinho").on("click", "li form p a", function(event){
+  var confirma = confirm("Tem certeza de que deseja exlcuir o item?")
+    if(confirma) {
+      $(this).closest("li").fadeOut("slow", function(){
+      $(this).remove();
+    });
+  }
+  
+  event.preventDefault();
+});
